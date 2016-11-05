57316581499f2023506b85581230990d8df2d861
Tiago Souza <tiagoice@hotmail.com>
2016-11-05 00:30:47 -0200

    Corrigindo problema com geracao do sequence

--- src/main/java/br/com/tiagods/model/CriarAdmin.java
+++ src/main/java/br/com/tiagods/model/CriarAdmin.java
@@ -13,64 +13,14 @@ public class CriarAdmin {
 	Usuario usuario;
 	Departamento departamento;
 	Funcao funcao;
+	
 	public CriarAdmin(){
 		Session session = new HibernateFactory().getSession();
 		int users = session.createQuery("from Usuario").getFirstResult();
-		session.getTransaction().commit();
 		if(users==0){
-			Departamento departamento = new Departamento();
-			departamento.setNome("Tecnologia");
-			int dps = session.createQuery("from Departamento as d where d.nome"
-					+ "= :nomeDepartamento ")
-					.setParameter("nomeDepartamento", departamento.getNome()).getFirstResult();
-			session.getTransaction().commit();
-			if(dps>0){
-				departamento = (Departamento) session.createQuery("from Departamento as d where d.nome"
-						+ "= :nomeDepartamento ")
-						.setParameter("nomeDepartamento", departamento.getNome()).getSingleResult();
-				session.getTransaction().commit();
-			}
-			else{
-				session.save(departamento);
-				session.getTransaction().commit();
-			}
-			
-			Funcao funcao = new Funcao();
-			funcao.setNome("Analista");
-			
-			String hql = "from Funcao as d where d.nome= :nomeFuncao";
-			int fun = session.createQuery(hql)
-					.setParameter("nomeFuncao", funcao.getNome()).getFirstResult();
-			session.getTransaction().commit();
-			
-			if(fun>0){
-				funcao = (Funcao) session.createQuery("from Funcao as d where d.nome"
-						+ "= :nomeFuncao ")
-						.setParameter("nomeFuncao", funcao.getNome()).getSingleResult();
-				session.getTransaction().commit();
-			}
-			else{
-				session.save(funcao);
-				session.getTransaction().commit();
-			}
-			usuario = new Usuario();
-			usuario.setLogin("admin");
-			usuario.setNome("Administrador");
-			usuario.setSenha("admin");
-			usuario.setEmail("suporte.ti@prolinkcontabil.com.br");
-			usuario.setCriadoEm(new Date());
-			usuario.setUltimoAcesso(new Date());
-			usuario.setDepartamento(departamento);
-			usuario.setFuncao(funcao);
-			usuario.setTotalVendas(new BigDecimal("0.00"));
-			try{
-				session.beginTransaction();
-				session.save(usuario);
-				session.getTransaction().commit();
-			}catch(HibernateException e){
-				e.printStackTrace();
-				session.getTransaction().rollback();
-			}	
+			criarDepartamento();
+			criarFuncao();
+			criarUsuario();
 		}
 		else{
 			usuario = session.find(Usuario.class, 1);
@@ -79,13 +29,57 @@ public class CriarAdmin {
 		session.close();
 	}
 	public void criarUsuario(){
-		
+		Session session = new HibernateFactory().getSession();
+		usuario = new Usuario();
+		usuario.setLogin("admin");
+		usuario.setNome("Administrador");
+		usuario.setSenha("admin");
+		usuario.setEmail("suporte.ti@prolinkcontabil.com.br");
+		usuario.setCriadoEm(new Date());
+		usuario.setUltimoAcesso(new Date());
+		usuario.setDepartamento(departamento);
+		usuario.setFuncao(funcao);
+		usuario.setTotalVendas(new BigDecimal("0.00"));
+		try{
+			session.save(usuario);
+			session.getTransaction().commit();
+		}catch(HibernateException e){
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}	
+		session.close();
 	}
 	public void criarDepartamento(){
-		
+		Session session = new HibernateFactory().getSession();
+		Departamento departamento = new Departamento();
+		departamento.setNome("Tecnologia");
+		try{
+			session.save(departamento);
+			session.getTransaction().commit();
+		}catch (HibernateException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
+		session.close();
 	}
-	
+	public void criarFuncao(){
+		Session session = new HibernateFactory().getSession();
+		Funcao funcao = new Funcao();
+		funcao.setNome("Analista");
+		try{
+		session.save(funcao);
+		session.getTransaction().commit();
+		}catch (HibernateException e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
+		session.close();
+	}
 	public Usuario getUsuario(){
+		Session session = new HibernateFactory().getSession();
+		usuario = session.find(Usuario.class, 1);
+		session.close();
 		return usuario;
 	}
 }
