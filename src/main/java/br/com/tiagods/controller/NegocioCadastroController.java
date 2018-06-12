d22475b2c70d7e1cc58847a01e9cff6115572e84
tiago <tiagoice@hotmail.com>
2018-06-12 19:34:30 -0300

    ids NegocioCadastro

--- src/main/java/br/com/tiagods/controller/NegocioCadastroController.java
+++ src/main/java/br/com/tiagods/controller/NegocioCadastroController.java
@@ -1,20 +1,127 @@
 package br.com.tiagods.controller;
 
 import java.net.URL;
-import java.util.AbstractMap;
 import java.util.ResourceBundle;
 
-import javax.persistence.Tuple;
+import org.controlsfx.control.Rating;
+import org.fxutils.maskedtextfield.MaskedTextField;
 
-import com.sun.prism.shader.Texture_ImagePattern_AlphaTest_Loader;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 
 import br.com.tiagods.modelcollections.NegocioProposta;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 public class NegocioCadastroController implements Initializable{
+	@FXML
+    private JFXRadioButton rbContato;
+
+    @FXML
+    private JFXRadioButton rbEnvioProposta;
+
+    @FXML
+    private JFXRadioButton rbFollowup;
+
+    @FXML
+    private JFXRadioButton rbFechamento;
+
+    @FXML
+    private JFXRadioButton rbIndefinida;
+
+    @FXML
+    private Rating rating;
+
+    @FXML
+    private Label lbrating;
+
+    @FXML
+    private JFXTextField txNome;
+
+    @FXML
+    private JFXRadioButton rbAndamento;
+
+    @FXML
+    private JFXRadioButton rbGanho;
+
+    @FXML
+    private JFXRadioButton rbPerdido;
+
+    @FXML
+    private JFXRadioButton rbIndefinido;
+
+    @FXML
+    private JFXDatePicker dtInicio;
+
+    @FXML
+    private JFXDatePicker dtFinal;
+
+    @FXML
+    private JFXTextField txHonorario;
+
+    @FXML
+    private JFXTextField txDescricao;
+
+    @FXML
+    private JFXComboBox<?> cbAtendente;
+
+    @FXML
+    private JFXComboBox<?> cbCategoria;
+
+    @FXML
+    private JFXComboBox<?> cbNivel;
+
+    @FXML
+    private JFXComboBox<?> cbOrigem;
+
+    @FXML
+    private JFXComboBox<?> cbServico;
+
+    @FXML
+    private JFXTextField txEmail;
+
+    @FXML
+    private MaskedTextField txTelefone;
+
+    @FXML
+    private MaskedTextField txCelular;
+
+    @FXML
+    private JFXTextField txSite;
+
+    @FXML
+    private JFXTextArea txResumo;
+
+    @FXML
+    private JFXTextArea txApresentacao;
+
+    @FXML
+    private JFXTextArea txDetalhesOrigem;
+
+    @FXML
+    private JFXTextField txValorServico;
+
+    @FXML
+    private TableView<?> tbServico;
+
+    @FXML
+    private TableView<?> tbListas1;
+
+    @FXML
+    private JFXTextField txNomeDocumento;
+
+    @FXML
+    private JFXTextField txLocalizacao;
+
+    @FXML
+    private TableView<?> tbTarefas;
 
 	private NegocioProposta proposta;
 	private Stage stage;
@@ -24,6 +131,16 @@ public class NegocioCadastroController implements Initializable{
 		this.proposta = proposta;
 		if(proposta!=null) preencherFormulario(proposta);
 	}
+	
+
+    @FXML
+    void anexarDocumento(ActionEvent event) {
+
+    }
+    @FXML
+    void cadastrarServico(ActionEvent event) {
+
+    }
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
@@ -32,9 +149,42 @@ public class NegocioCadastroController implements Initializable{
 	private void preencherFormulario(NegocioProposta proposta) {
 		
 	}
-	@FXML
-	void sair(ActionEvent event) {
-		stage.close();
-	}
+    @FXML
+    void mailSend(ActionEvent event) {
 
+    }
+
+    @FXML
+    void novaTarefa(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openBrowser(ActionEvent event) {
+
+    }
+    @FXML
+    void sair(ActionEvent event) {
+    	stage.close();
+    }
+
+    @FXML
+    void salvar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salvarDocumento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salvarServico(ActionEvent event) {
+
+    }
+
+    @FXML
+    void serviçosAdicionais(ActionEvent event) {
+
+    }
 }
--- src/main/java/br/com/tiagods/model/NegocioMotivoPerdaTrio.java
+++ src/main/java/br/com/tiagods/model/NegocioMotivoPerdaTrio.java
@@ -1,22 +1,56 @@
 package br.com.tiagods.model;
 
-public class NegocioMotivoPerdaTrio<MotivoPerda,Calendar,String>{
-		private MotivoPerda motivo;
-		private Calendar dataPerda;
-		private String detalhes;
+public class NegocioMotivoPerdaTrio<T,U,V>{
+		private T value1;
+		private U value2;
+		private V value3;
 		
-		public NegocioMotivoPerdaTrio(MotivoPerda motivo, Calendar dataPerda, String detalhes) {
-			this.motivo=motivo;
-			this.dataPerda=dataPerda;
-			this.detalhes=detalhes;
+		public NegocioMotivoPerdaTrio(T value1, U value2, V value3) {
+			this.value1=value1;
+			this.value2=value2;
+			this.value3=value3;
 		}
-		public Calendar getDataPerda() {
-			return dataPerda;
+
+		/**
+		 * @return the value1
+		 */
+		public T getValue1() {
+			return value1;
 		}
-		public String getDetalhes() {
-			return detalhes;
+
+		/**
+		 * @param value1 the value1 to set
+		 */
+		public void setValue1(T value1) {
+			this.value1 = value1;
 		}
-		public MotivoPerda getMotivo() {
-			return motivo;
+
+		/**
+		 * @return the value2
+		 */
+		public U getValue2() {
+			return value2;
 		}
+
+		/**
+		 * @param value2 the value2 to set
+		 */
+		public void setValue2(U value2) {
+			this.value2 = value2;
+		}
+
+		/**
+		 * @return the value3
+		 */
+		public V getValue3() {
+			return value3;
+		}
+
+		/**
+		 * @param value3 the value3 to set
+		 */
+		public void setValue3(V value3) {
+			this.value3 = value3;
+		}
+		
 	}
