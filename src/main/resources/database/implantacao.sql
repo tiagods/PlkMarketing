78a5c976e3fedf9c5f6ffec746d1b5ecf829ae75
Tiago Souza <tiagoice@hotmail.com>
2019-02-08 15:15:07 -0200

    sql de implantacao

--- src/main/resources/database/implantacao.sql
+++ src/main/resources/database/implantacao.sql
@@ -38,3 +38,52 @@ ALTER TABLE imp_atividade OWNER TO prolink;
 ALTER TABLE imp_pacote OWNER TO prolink;
 ALTER TABLE imp_pac_etapa OWNER TO prolink;
 
+create table imp_processo(
+	id serial,
+	cliente_id integer,
+	criado_por_id integer,
+	criado_em timestamp,
+	primary key(id)
+);
+
+alter table imp_processo add constraint fk_cliente_id foreign key (cliente_id) references cliente(COD);
+
+alter table imp_processo add constraint fk_criado_por_id foreign key (criado_por_id) references usuario(id);
+
+ALTER TABLE imp_processo OWNER TO prolink;
+
+create table imp_pro_etapa(
+	id serial,
+	etapa varchar,
+	departamento_id integer,
+	tempo integer,
+	descricao varchar,
+	status boolean,
+	atividade_id integer,
+	processo_id integer,
+	criado_em timestamp,
+    criado_por_id integer,
+	primary key(id)	
+	
+);
+
+alter table imp_pro_etapa add constraint fk_departamento_id foreign key (departamento_id) references USUARIO_DEPARTAMENTO(USU_DEP_COD);
+alter table imp_pro_etapa add constraint fk_atividade_id foreign key (atividade_id) references imp_atividade(id);
+alter table imp_pro_etapa add constraint fk_processo_id foreign key (processo_id) references imp_processo(id);
+
+ALTER TABLE imp_pro_etapa OWNER TO prolink;
+
+create table imp_pro_eta_status(
+	id serial,
+	criado_por_id integer,
+	criado_em timestamp,
+	status boolean,
+	processo_etapa_id integer,
+	descricao text,
+	primary key(id)
+);
+
+alter table imp_pro_eta_status add constraint fk_criado_por_id foreign key (criado_por_id) references usuario(id);
+alter table imp_pro_eta_status add constraint fk_processo_etapa_id foreign key (processo_etapa_id) references imp_pro_etapa(id);
+
+ALTER TABLE imp_pro_eta_status OWNER TO prolink;
